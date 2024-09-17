f3267a7c85819af52c5d8fb3e83b9bd2732123b6
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-09-17 10:16:31 +0700

    update handler and notes

---
+++ src/handler.js
@@ -0,0 +1,26 @@
+const { nanoid } = require('nanoid');
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+  notes.push(newNote);
+};
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+module.exports = { addNoteHandler, getAllNotesHandler };
